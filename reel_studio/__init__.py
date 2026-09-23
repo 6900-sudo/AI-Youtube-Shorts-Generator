f88be00bc17123c8ba775fc2172ff825cec9d3ab
Stephen Hughes <paulhughes6900@gmail.com>
2026-09-23 03:36:30 +0100

    feat(reel-studio): add package exports

---
+++ reel_studio/__init__.py
@@ -0,0 +1,5 @@
+"""Speech-first cinematic reel production primitives."""
+
+from .manifest import build_timing_contract, load_manifest, validate_manifest
+
+__all__ = ["build_timing_contract", "load_manifest", "validate_manifest"]
